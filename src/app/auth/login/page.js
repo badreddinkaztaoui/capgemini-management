a152be6df1c112dd1d9a466ef4239dd7a3f138c3
badreddinkaztaoui <kaztaouibadreddin@gmail.com>
2025-05-13 23:03:40 +0100

    Fix deployment issue

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -159,7 +159,7 @@ export default function Login() {
 
         <div className="text-center mt-6">
           <p className="text-sm text-gray-600">
-            Don't have an account?{" "}
+            Don&apos;t have an account?{" "}
             <Link
               href="/auth/register"
               className="font-medium text-indigo-600 hover:text-indigo-500"
